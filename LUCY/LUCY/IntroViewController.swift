edcee6f507b36676f4c788741f2e7492c467176c
Cosmos <applepiefish@gmail.com>
2015-05-22 18:01:34 -0700

    Added video player and AVFoundation + MediaPlayer frameworks

--- LUCY/LUCY/IntroViewController.swift
+++ LUCY/LUCY/IntroViewController.swift
@@ -7,18 +7,56 @@
 //
 
 import UIKit
-
+import MediaPlayer
 class IntroViewController: UIViewController {
 
+    var moviePlayer: MPMoviePlayerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.playVideo();
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func playVideo() ->Bool {
+        
+        //take path of video
+        let pathURL = NSURL(fileURLWithPath: "background.mp4");
+                
+        moviePlayer = MPMoviePlayerController(contentURL: pathURL)
+        //asigning video to moviePlayer
+        
+        if let player = moviePlayer {
+            player.view.frame = self.view.bounds
+            //setting the video size to the view size
+            
+            player.controlStyle = MPMovieControlStyle.None
+            //Hiding the Player controls
+            
+            
+            player.prepareToPlay()
+            //Playing the video
+            
+            
+            player.repeatMode = .One
+            //Repeating the video
+            
+            player.scalingMode = .AspectFill
+            //setting the aspect ratio of the player
+            
+            self.view.addSubview(player.view)
+            //adding the player view to viewcontroller
+            return true
+            
+        }
+        return false
     }
     
     
