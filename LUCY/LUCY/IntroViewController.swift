c2a9b47a3b1faec7e35946b92d311ab93df555b6
Will Gu <willgu29@gmail.com>
2015-05-22 17:53:01 -0700

    Initial Version

---
+++ LUCY/LUCY/IntroViewController.swift
@@ -0,0 +1,26 @@
+//
+//  IntroViewController.swift
+//  LUCY
+//
+//  Created by William Gu on 5/22/15.
+//  Copyright (c) 2015 William Gu. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
